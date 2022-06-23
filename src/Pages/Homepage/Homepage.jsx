6107d35d1ca1974855c4238d4e049db815a365f4
Kritika176 <kritikash944@gmail.com>
2022-06-23 12:38:09 +0530

    searchBox ui completed

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,20 +1,30 @@
-   import style from './Homepage.module.css';
-   import LocationOnIcon from '@mui/icons-material/LocationOn';
-   export const Homepage = () => {
-     return (
-       <>
-            <div className={style.homepage}>
-                <input className={style.searchBox}>
-                  
-                {/* <div className={style.searchLocation}>
-                <div className={style.searchIcon}><LocationOnIcon style={{"height":"100%","width":"100%"}}></LocationOnIcon></div>
-                <div className={style.searchLocationText}>Search Location</div>
-                </div> */}
-                </input>
-               
-            </div> 
-       </>
-     )
-   }
-   
-   
+import style from "./Homepage.module.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import SearchIcon from "@mui/icons-material/Search";
+export const Homepage = () => {
+  return (
+    <>
+      <div className={style.homepage}>
+        <div className={style.searchDiv}>
+          <div className={style.locationIcon}>
+            {" "}
+            <LocationOnIcon
+              style={{ width: "100%", height: "100%" }}
+            ></LocationOnIcon>
+          </div>
+          <input className={style.searchInput}></input>
+          <div className={style.searchIcon}>
+            <SearchIcon
+              style={{
+                position: "absolute",
+                top: "4%",
+                width: "100%",
+                height: "100%",
+              }}
+            ></SearchIcon>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
